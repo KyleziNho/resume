51ab9c3720f044f195f1ec1e4c7388a6547f90ba
Your Name <kos33@bath.ac.uk>
2026-02-10 16:07:11 +0000

    Navigation changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/valentines",
+        destination: "/valentines.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
